38a7aa936400ce65d009cde59d682cfb34ce2313
Hajime Shiozawa <hajime.shiozawa@gmail.com>
2025-10-13 23:10:00 +0900

    feat: add binary code

--- 04/Fill/Fill.asm
+++ 04/Fill/Fill.asm
@@ -3,9 +3,62 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+(START)
+    // COLOR=0
+    @0
+    D=A
+    @COLOR
+    M=D
+	// if (KBD == 0) goto @RENDER;
+    @KBD
+    D=M
+    @RENDER
+    D;JEQ
+	// COLOR=-1
+	@COLOR
+	M=-1
+(RENDER)
+    // n=0
+    @0
+    D=A
+    @n
+    M=D
+(LOOP)
+    // if (n - 8192 == 0) goto @START; 
+    @n
+	D=M
+    @8192
+	D=D-A
+    @START
+	D;JEQ
+	// ADDR = (SCREEN + n)
+	@n
+	D=M
+	@SCREEN
+	D=D+A
+    @ADDR
+    M=D
+	// *(ADDR) = COLOR
+	@COLOR
+	D=M
+	@ADDR
+	A=M
+	M=D
+	// n += 1
+	@1
+	D=A
+	@n
+	M=D+M
+	@LOOP
+	0;JMP
+	
+	
+	
+	
+    
--- 04/Mult/Mult.asm
+++ 04/Mult/Mult.asm
@@ -8,3 +8,37 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+    // R2=0
+	@0
+	D=A
+	@R2
+	M=D
+	// n=R1
+	@R1
+	D=M
+	@n
+	M=D
+(LOOP)
+    // if (n == 0) goto @END;
+	@n
+	D=M
+	@END
+	D;JEQ
+	// R2 += R0
+	@R0
+	D=M
+	@R2
+	M=D+M
+	// n -= 1
+	@n
+	D=M
+	@1
+	D=D-A
+	@n
+	M=D
+	// jump to
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
